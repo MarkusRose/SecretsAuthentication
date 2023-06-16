64e7ebf5f47fd54bd5124171b78f6ea09bc61871
Markus Rose <markus.m.rose@gmail.com>
2023-06-16 23:09:26 +0200

    initial commit of authentication

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', (req, res) => {
+    res.render("home");
+})
+
+app.get('/login', (req, res) => {
+    res.render("login");
+})
+app.get('/register', (req, res) => {
+    res.render("register");
+})
+
+app.post('/login', (req, res) => {
+    const newUser = {
+        name: req.body.username,
+        password: req.body.password,
+    }
+    console.log(newUser);
+    res.redirect('/');
+})
+
+app.post('/register', (req, res) => {
+    const newUser = {
+        name: req.body.username,
+        password: req.body.password,
+    }
+    console.log(newUser);
+    res.redirect('/');
+})
+
+app.listen(3000, () => {console.log("Server stared on port 3000.");});
