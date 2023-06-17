cd7041cfef7d74335e0abe42cb2014f2e720cf47
Markus Rose <markus.m.rose@gmail.com>
2023-06-17 22:18:22 +0200

    add code formatting to project

--- app.js
+++ app.js
@@ -1,42 +1,45 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get('/', (req, res) => {
-    res.render("home");
-})
+  res.render('home');
+});
 
 app.get('/login', (req, res) => {
-    res.render("login");
-})
+  res.render('login');
+});
 app.get('/register', (req, res) => {
-    res.render("register");
-})
+  res.render('register');
+});
 
 app.post('/login', (req, res) => {
-    const newUser = {
-        name: req.body.username,
-        password: req.body.password,
-    }
-    console.log(newUser);
-    res.redirect('/');
-})
+  const newUser = {
+    name: req.body.username,
+    password: req.body.password,
+  };
+  console.log(newUser);
+  res.redirect('/');
+});
 
 app.post('/register', (req, res) => {
-    const newUser = {
-        name: req.body.username,
-        password: req.body.password,
-    }
-    console.log(newUser);
-    res.redirect('/');
-})
+  const newUser = {
+    name: req.body.username,
+    password: req.body.password,
+  };
+  console.log(newUser);
+  res.redirect('/');
+});
 
-app.listen(3000, () => {console.log("Server stared on port 3000.");});
+app.listen(3000, () => {
+  console.log('Server stared on port 3000.');
+});
