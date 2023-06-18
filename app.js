030ed1edc2a812446e746745fb3b672a677823ad
Markus Rose <markus.m.rose@gmail.com>
2023-06-18 13:50:05 +0200

    connecting to mongodb atlas and storing secrets outside git

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const secrets = require('./secrets');
+
+mongoose.connect(
+  `mongodb+srv://${secrets.admin}:${secrets.password}@sandbox.diqjrsw.mongodb.net/userTest?retryWrites=true&w=majority`,
+  {}
+);
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model('User', userSchema);
 
 const app = express();
 
@@ -21,23 +35,51 @@ app.get('/login', (req, res) => {
 app.get('/register', (req, res) => {
   res.render('register');
 });
+app.get('/submitted', (req, res) => res.render('submit'));
+app.get('/secrets', (req, res) => {
+  res.render('secrets');
+});
 
 app.post('/login', (req, res) => {
-  const newUser = {
-    name: req.body.username,
-    password: req.body.password,
-  };
-  console.log(newUser);
-  res.redirect('/');
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username })
+    .then((user) => {
+      if (!user || user.password !== password) {
+        console.log('Wrong username or password!');
+        res.redirect('/');
+      } else {
+        res.redirect('/secrets');
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/');
+    });
 });
 
 app.post('/register', (req, res) => {
-  const newUser = {
-    name: req.body.username,
-    password: req.body.password,
-  };
-  console.log(newUser);
-  res.redirect('/');
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }).then((user) => {
+    if (user) {
+      res.redirect('/submitted');
+    } else {
+      const newUser = new User({
+        email: username,
+        password,
+      });
+      newUser
+        .save()
+        .then(() => {
+          res.redirect('/submitted');
+        })
+        .catch((err) => {
+          console.log(err);
+          res.redirect('/');
+        });
+    }
+  });
 });
 
 app.listen(3000, () => {
